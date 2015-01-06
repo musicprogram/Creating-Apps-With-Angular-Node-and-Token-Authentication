dd5ccef11c5f93b447d465078e06b66893bb04ec
JuanH <juan-o-henao@hotmail.es>
2015-01-05 20:07:08 -0500

    Primer commit video 8 ui-router

---
+++ app/scripts/app.config.js
@@ -0,0 +1,18 @@
+angular.module('angularNodeApp').config(function($urlRouterProvider, $stateProvider){
+
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+
+	.state('main',{
+		url:'/',
+		templateUrl: '/views/main.html'
+	})
+
+
+	.state('register',{
+		url:'/register',
+		templateUrl: '/views/register.html'
+	});
+
+})
---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularNodeApp
+ * @description
+ * # angularNodeApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularNodeApp', ['ui.router']);
